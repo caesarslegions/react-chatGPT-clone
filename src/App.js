388039dd2c19ff3ef7568472098ded12d5b94e65
Jonah Popp <jonah.popp@gmail.com>
2023-05-04 18:37:28 -0500

    Initial commit. Unnecessary files removed and basic HTML structure implemented.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="side-bar"></section>
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Jonah</p>
+        </nav>
+      <section className="main"></section>
     </div>
   );
 }
