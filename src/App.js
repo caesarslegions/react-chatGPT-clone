6ceedc1923a4bdd8c122a67d1e669cf03f156d8f
Jonah Popp <jonah.popp@gmail.com>
2023-05-04 19:58:23 -0500

    Added styling elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 
 const App = () => {
   return (
-    <div className="App">
-      <section className="side-bar"></section>
-        <button>+ New chat</button>
-        <ul className="history"></ul>
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>BLUGH</li>
+        </ul>
         <nav>
           <p>Made by Jonah</p>
         </nav>
-      <section className="main"></section>
+      </section>
+      <section className="main">
+        <h1>LennyGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May 3 Version</p>
+        </div>
+      </section>
     </div>
   );
 }
