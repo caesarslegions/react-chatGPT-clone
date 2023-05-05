32592aa5096f625c11c32db64ece493f92b458a7
Jonah Popp <jonah.popp@gmail.com>
2023-05-05 00:32:19 -0500

    User interface and API integration complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
+import {useState, useEffect} from 'react'
 
 const App = () => {
+  const [value, setValue] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
+  const [currentTitle, setCurrentTitle] = useState(null)
+
+  const createNewChat = () => {
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle()
+    setMessage(null)
+    setValue("")
+  }
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value)
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats(prevChats => (
+        [...prevChats, {
+          title: currentTitle,
+          role: "user",
+          content: value
+        }, {
+          title: currentTitle,
+          role: message.role,
+          content: message.content
+        }]
+      ))
+    }
+  }, [message, currentTitle])
+
+  console.log(previousChats)
+
+  const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
+  const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
+  console.log(uniqueTitles)
+
   return (
     <div className="app">
       <section className="side-bar">
-        <button>+ New Chat</button>
+        <button onClick={createNewChat}>+ New Chat</button>
         <ul className="history">
-          <li>BLUGH</li>
+          {uniqueTitles?.map((uniqueTitle, index) =><li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
         </ul>
         <nav>
           <p>Made by Jonah</p>
         </nav>
       </section>
       <section className="main">
-        <h1>LennyGPT</h1>
+        {!currentTitle && <h1>LennyGPT</h1>}
         <ul className="feed">
-
+          {currentChat?.map((chatMessage, index) => <li key={index}>
+            <p className="role">{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input/>
-            <div id="submit">➢</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May 3 Version</p>
         </div>
